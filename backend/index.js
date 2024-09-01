ed7392c17235dd0bc174f7c9db92f117c309e46d
Usman-17 <usmandev580@gmail.com>
2024-09-01 23:53:24 +0500

    Implement Cloudinary Integration for Media Storage

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+
 import dbConnect from "./db/ConnectMongoDB.js";
+import dotenv from "dotenv";
+
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 // imports End
@@ -9,9 +13,22 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //to parse from data(urlencoded)
 app.use(cookieParser());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 // Routes Setup
 app.use("/api/auth", authRoutes);
