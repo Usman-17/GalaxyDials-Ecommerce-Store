a21b15170231fda9f20a01d34a6e4baab6d52d65
Usman-17 <usmandev580@gmail.com>
2024-09-03 20:50:51 +0500

    Implement Get All Users Functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,3 +16,22 @@ export const getUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+// PATH     : /api/user/
+// METHOD   : GET
+// ACCESS   : PRIVATE
+// DESC     : Get All the Users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    if (!users.length === 0) {
+      return res.status(404).json({ error: "No users found" });
+    }
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error in getAllUsers Controller:", error.message);
+    res.status(500).json({ error: "Server Error" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,8 +3,9 @@ import express from "express";
 const router = express.Router();
 
 import { protectRoute } from "../middlewares/authMiddleware.js";
-import { getUser } from "../controllers/user.controller.js";
+import { getAllUsers, getUser } from "../controllers/user.controller.js";
 
 router.get("/:id", protectRoute, getUser);
+router.get("/", protectRoute, getAllUsers);
 
 export default router;
