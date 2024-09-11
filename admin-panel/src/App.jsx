677354f5891912e0a7ba6b01020aca8562039c6c
Usman-17 <usmandev580@gmail.com>
2024-09-11 22:51:30 +0500

    feat: implement React Router DOM for routing

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
 import SideBar from "./components/common/SideBar";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <SideBar />
-    </>
+    </BrowserRouter>
   );
 };
 
