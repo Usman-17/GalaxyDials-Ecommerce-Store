099e2b42e00187fd3214d51babac176c3e3a7ce7
Usman-17 <usmandev580@gmail.com>
2024-09-12 23:11:19 +0500

    feat: setup TanStack Query for efficient data fetching from backend

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import "./index.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
