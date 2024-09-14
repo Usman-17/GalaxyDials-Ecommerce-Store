7e8e634b3da10c805808341a7addede0430c6f18
Usman-17 <usmandev580@gmail.com>
2024-09-14 18:11:14 +0500

    feat: fetch and display authenticated user, handle routing based on auth state

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,19 +1,56 @@
 import "./App.scss";
 import Header from "./components/common/Header";
 import SideBar from "./components/common/SideBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import UsersPage from "./pages/UsersPage";
 import LoginPage from "./pages/LoginPage";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { Spin } from "antd";
 
 const App = () => {
+  // fetch Authentication User Data
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if (data.error) return null;
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="min-vh-100 d-flex align-items-center justify-content-center">
+        <Spin size="large" />
+      </div>
+    );
+  }
   return (
     <BrowserRouter>
-      <SideBar />
-      <Header />
+      {authUser && (
+        <>
+          <SideBar />
+          <Header />
+        </>
+      )}
 
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
         <Route path="/users" element={<UsersPage />} />
       </Routes>
 
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.user_jwt;
+    const token = req.cookies.admin_jwt;
 
     if (!token) {
       return res.status(401).json({ error: "Unauthorized: No Token Provided" });
