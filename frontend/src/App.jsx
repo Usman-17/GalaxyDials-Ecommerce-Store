9eecdbabe2802ba149e57ff67b8076d13ee26fdd
Usman-17 <usmandev580@gmail.com>
2024-09-20 00:03:44 +0500

    feat: implement React Router DOM for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 const App = () => {
-  return <h1 className="text-6xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
