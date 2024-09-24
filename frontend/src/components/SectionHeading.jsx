1becbd473383ef0a6516b3595b27b80ea8dac243
Usman-17 <usmandev580@gmail.com>
2024-09-24 21:58:01 +0500

    feat: implement responsive slick slider arrows

--- frontend/src/components/SectionHeading.jsx
+++ frontend/src/components/SectionHeading.jsx
@@ -1,7 +1,7 @@
 const SectionHeading = ({ text1, text2 }) => {
   return (
     <div className="inline-flex gap-2 items-center mb-3">
-      <p className="text-gray-500">
+      <p className="text-gray-500 text-lg sm:text-2xl">
         {text1} <span className="text-gray-700 font-medium">{text2}</span>
       </p>
 
