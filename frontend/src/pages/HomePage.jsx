23ce8baeca7d7da6d481760c5b2cd8765b282468
Usman-17 <usmandev580@gmail.com>
2024-09-25 00:07:19 +0500

    feat: Enhance SEO with Helmet for homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import Hero from "../components/Hero";
 import ProductCard from "../components/ProductCard";
 import ProductSlider from "../components/ProductSlider";
@@ -7,6 +9,19 @@ import OurPolicy from "../components/OurPolicy";
 const HomePage = () => {
   return (
     <>
+      <Helmet>
+        <title>Galaxy Dials | Luxury Watches for Every Occasion</title>
+        <meta
+          name="description"
+          content="Explore Galaxy Dials for a curated collection of luxury watches. Enjoy free shipping on orders over Rs.5,000, exclusive deals, and the latest trends in timepieces. Shop premium brands and find the perfect watch for every occasion."
+        />
+        <meta
+          name="keywords"
+          content="luxury watches, Galaxy Dials, designer watches, automatic watches, watch trends, premium timepieces, free shipping , buy watches online"
+        />
+        <link rel="canonical" href="" />
+      </Helmet>
+
       <Hero />
       <div className="mt-10">
         <div className="py-1 text-3xl">
