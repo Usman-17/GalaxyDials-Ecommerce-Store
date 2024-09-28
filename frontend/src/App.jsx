a3479c06df7e89dbd715973f02954201b6ecdc65
Usman-17 <usmandev580@gmail.com>
2024-09-28 23:20:50 +0500

    feat: Add scroll to top feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,9 +9,20 @@ import CollectionPage from "./pages/CollectionPage";
 // imports End
 
 const App = () => {
+  // Scroll to Top Component
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  };
   return (
     <div className="px-4 sm:px-[5vw] md:px-[3vw] lg:px-[3vw]">
       <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
