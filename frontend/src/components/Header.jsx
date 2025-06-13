e185770e47aeac056387e87f602f10ef33413bac
Usman-17 <usmandev580@gmail.com>
2025-06-13 18:49:04 +0500

    style: enhance header responsiveness for mobile and tablet

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
   };
 
   return (
-    <div className="px-1 sm:px-[5vw] md:px-[7vw]  xl:px-[0.5vw] 2xl:xl:px-[1vw]">
+    <div className="px-1 sm:px-[5vw] md:px-[1vw] xl:px-[0.5vw] 2xl:xl:px-[1vw]">
       <div className="flex items-center justify-between py-2.5 sm:py-3 font-medium">
         <Link to={"/"}>
           <img
