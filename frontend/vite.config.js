01bf69e46cc9760412dfe4780b05a240a2cd3a70
Usman-17 <usmandev580@gmail.com>
2025-06-15 14:50:04 +0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://galaxydials-ecommerce-store.onrender.com/",
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
