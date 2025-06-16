de857965518bd94e53e04fa8d16edfe8740fea11
Usman-17 <usmandev580@gmail.com>
2025-06-16 13:05:37 +0500

    fix: improve placement of clear button in search model for better UX

--- frontend/src/components/SearchModal.jsx
+++ frontend/src/components/SearchModal.jsx
@@ -83,7 +83,7 @@ const SearchModal = ({ onClose }) => {
            ${visible ? "translate-y-0" : "translate-y-full"} `}
       >
         {/* Close Search Button */}
-        <div className="flex justify-end p-2 sm:p-4">
+        <div className="flex justify-end p-3 sm:p-4">
           <X
             className="cursor-pointer text-gray-700 hover:text-black transition-transform duration-150 hover:rotate-90"
             size={24}
@@ -111,7 +111,7 @@ const SearchModal = ({ onClose }) => {
           {search && (
             <button
               onClick={handleClear}
-              className="absolute right-3  sm:right-8 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+              className="absolute right-7  sm:right-8 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
               aria-label="Clear search"
             >
               <X size={22} />
